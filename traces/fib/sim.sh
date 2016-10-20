f01b1d2b18c663302d892aa93d4e4c30fcc38e7d
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2016-10-20 15:34:43 -0700

    [trace] fib: initialize some registers with dummy values

--- traces/fib/sim.sh
+++ traces/fib/sim.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-../sim-trace.py pc target-fw.elf sp=0x10000bc0
+# initialize the r4,r5,r6,lr registers to some dummy value
+# as the call to fib in main will save and restore them
+../sim-trace.py pc target-fw.elf sp=0x10000bc0 r14=0 r6=0 r5=0 r4=0
