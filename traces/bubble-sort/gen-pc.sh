c6b3acc64d621276f051765715a999d20e552c5b
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2016-10-28 20:05:13 -0700

    [traces] bubble: add complete trace

--- traces/bubble-sort/gen-pc.sh
+++ traces/bubble-sort/gen-pc.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
 # use sigrok to interpret the ETM output and extract pc values
-sigrok-cli -i trace.sr -P uart:baudrate=8000000:rx=D7,arm_tpiu:stream=10,arm_etmv3:branch_enc=original -S uart,arm_tpiu,arm_etmv3 -A arm_etmv3=pc > pc
+sigrok-cli -i first_complete_trace.sr -P uart:baudrate=16000000:rx=D7,arm_tpiu:stream=10:sync_offset=7,arm_etmv3:branch_enc=original -S uart,arm_tpiu,arm_etmv3 -A arm_etmv3=pc > pc
 
+# please not that this has only been tested with a patched version of
+# the sigrok arm_tpiu decoder
+# in this version I removed the code that resets the buffer after a
+# long silence
