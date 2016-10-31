aa06eb691c0fd3ec5afb5f1e220f1760677a65d2
Kevin Laeufer <kevin.laeufer@rwth-aachen.de>
2016-10-30 21:39:33 -0700

    [traces] bubble: init some registers that get pushed before they are written

--- traces/bubble-sort/sim.sh
+++ traces/bubble-sort/sim.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-
-../sim-trace.py pc target.elf r3=0 lr=0
+# r3,r5,r6,r7 are pushed to the stack before they are
+# accessed for the first time
+../sim-trace.py pc target.elf r3=0 lr=0 r7=0 r6=0 r5=0
